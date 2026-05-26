04586ead6437c9acd1d47b71ad4bcc7ccd456369
OwenBingley <owenbingley10@gmail.com>
2026-05-26 18:26:37 +0000

    save_10

--- src/main/java/platformer/code/gamelogic/tiles/Gas.java
+++ src/main/java/platformer/code/gamelogic/tiles/Gas.java
@@ -6,20 +6,21 @@ import platformer.code.gameengine.hitbox.RectHitbox;
 import platformer.code.gamelogic.level.Level;
 
 public class Gas extends Tile{
+    private int intensity;
+    public Gas(float x, float y, int size, BufferedImage image, Level level, int intensity) {
+   	 super(x, y, size, image, false, level);
+   	 this.intensity = intensity;
+   	 this.hitbox = new RectHitbox(x*size , y*size, 0, 10, size, size);
+    }
+    public int getIntensity() {
+   	 return intensity;
+    }
+    public void setIntensity(int intensity) {
+   	 this.intensity = intensity;
+    }
+    public void update(float tslf, BufferedImage image) {
+   	 super.update(tslf);
+   	 super.setImage(image);
+    }
+} 
 
-	private int intensity;
-	
-	public Gas(float x, float y, int size, BufferedImage image, Level level, int intensity) {
-		super(x, y, size, image, false, level);
-		this.intensity = intensity;
-		this.hitbox = new RectHitbox(x*size , y*size, 0, 10, size, size);
-	}
-	
-	public int getIntensity() {
-		return intensity;
-	}
-	
-	public void setIntensity(int intensity) {
-		this.intensity = intensity;
-	}
-}
